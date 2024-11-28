dc0f399fd86e6749d94c22a8407b9bcfa37106b4
Noya Birman <noya.birman@post.runi.ac.il>
2024-11-28 19:34:01 +0200

    completed

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,55 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String new1= preProcess(str1);
+		String new2= preProcess(str2);
+
+		if (new1.length() != new2.length()) {
+            return false;
+		}
+
+		for (int i=0; i<new1.length();i++){
+			boolean identical= false;
+			for (int j=0; j<new2.length();j++){
+				if (new1.charAt(i)==new2.charAt(j)){
+				new2=new2.substring(0,j)+new2.substring(j+1);
+				identical= true;
+				break;
+
+			}
+		}
+		if (!identical){
+			return false;
+		}
+	}
+	return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String pre= "";
+		for (int i=0; i<str.length();i++){
+			if (Character.isLetter(str.charAt(i))){
+				pre+=Character.toLowerCase(str.charAt(i));
+			}
+		}
+		return pre;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String randomAnagram="";
+		String processed= preProcess(str);
+		
+		while (processed.length() > 0) {
+				int randomIndex= (int)(Math.random()* processed.length());
+				randomAnagram+=processed.charAt(randomIndex);
+				processed = processed.substring(0, randomIndex) + processed.substring(randomIndex + 1);
+			} 
+			return randomAnagram;
+		}
 	}
-}
+
